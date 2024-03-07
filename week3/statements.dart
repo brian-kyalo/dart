447cb2355a230f06c63bb0f2996399519e7866b9
brian-kyalo <brianmuthama15@gmail.com>
2024-03-07 19:38:59 +0300

    week 3 assignment

---
+++ week3/statements.dart
@@ -0,0 +1,18 @@
+/*void main(){
+    var age = 4;
+    if(age>18){
+        print("You are allowed to vote");
+    }else{
+        print("You are not allowed to vote in kenya");
+    }
+}*/
+
+void main(){
+    var a = 1;
+    var max = 11;
+    while(a<max){
+        a= a +1;
+        print(a);
+        
+    }
+}
